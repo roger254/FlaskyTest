d234c083e5573f2242c603a343ac38fc52d3d34d
roger254 <rogerbrian.mutua@gmail.com>
2018-11-26 17:48:30 +0300

    user level profile editor added

--- app/main/views.py
+++ app/main/views.py
@@ -48,15 +48,19 @@ def user(username):
 @main.route('/edit-profile', methods=['GET', 'POST'])
 @login_required
 def edit_profile():
+
     form = EditProfileForm()
     if form.validate_on_submit():
+
         current_user.name = form.name.data
         current_user.location = form.location.data
         current_user.about_me = form.about_me.data
         db.session.add(current_user._get_current_object())
         db.session.commit()
+
         flash('Your profile has been updated.')
         return redirect(url_for('.user', username=current_user.username))
+
     form.name.data = current_user.name
     form.location.data = current_user.location
     form.about_me.data = current_user.about_me
