87b2f0210481fa842cb72f31d5004a4d396a4ea7
roger254 <rogerbrian.mutua@gmail.com>
2018-11-27 09:54:26 +0300

    added profile block

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from datetime import datetime
-from flask import render_template, session, redirect, url_for,\
-    current_app, flash
+from flask import render_template, redirect, url_for,\
+    abort, flash
 from flask_login import login_required, current_user
 from app.email import send_email
 from . import main
@@ -36,8 +36,13 @@ def index():
 
 @main.route('/user/<username>')
 def user(username):
-    user = User.query.filter_by(username=username).first_or_404()
-    return render_template('user.html', user=user)
+    user = User.query.filter_by(username=username).first()
+
+    if user is None:
+        abort(404)
+
+    posts = user.posts.order_by(Post.timestamp.desc()).all()
+    return render_template('user.html', user=user, posts=posts)
 
 
 @main.route('/edit-profile', methods=['GET', 'POST'])
